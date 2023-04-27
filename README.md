3973f5ffcb0d1f1019b49e59534804c9a6daadb2
Noah Yao <noahyao1024@gmail.com>
2023-04-27 21:23:11 +0800

    Fix run script.

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
-# Run a simple Web Server like spitting.
+# Run a simple Web Server like Spitting <img src="https://www.emojirequest.com/images/SpittingEmoji.jpg" width="40" height="40"><img>
+
+Spit is a simple web server that can be used to proxy API requests to an upstream server. Here are some quick start instructions.
 
 # Quick start
 
-Run a server on port 1080, and proxy some API to upstream server.
+- Run a simple HTTP server at `80` port, `static` is the html folder.
+  `./run.sh start`, Visit [DEMO](http://localhost/noah.html).
 
-`./run.sh start 1080 proxyapi 192.168.5.1:8080`
+- Run a simple HTTP server with specify port(`8888` here).
+  `./run.sh start 8888`
 
-Run in `Daemon` mode.
+- Run a simple HTTP server with some specify port and proxy backend APIs(begin with `proxyapi`)
+  `./run.sh start 8888 proxyapi 192.168.5.1:8080`
 
-`./run.sh daemon 1080 proxyapi 192.168.5.1:8080`
+- Run in `Daemon` mode.
+  `./run.sh daemon 8888 proxyapi 192.168.5.1:8080`
 
-Stop daemon mode.
-
-`./run.sh stop`
+- Stop daemon mode.
+  `./run.sh stop`
--- run.sh
+++ run.sh
@@ -3,14 +3,9 @@
 # Usage: ./script.sh [start|stop|daemon] [port_number] [upstream_pattern] [upstream_host]
 DAEMON=false
 ACTION=$1
-PORT=$2
-UPSTREAM_PATTERN=$3
-UPSTREAM_HOST=$4
-
-# Check if a valid port number was provided
-if [[ -z "$PORT" ]] || ! [[ "$PORT" =~ ^[0-9]+$ ]]; then
-  PORT=80
-fi
+PORT=${2:-80}
+UPSTREAM_PATTERN=${3:-blackhole}
+UPSTREAM_HOST=${4:-1.1.1.1}
 
 # Determine the operating system and architecture
 OS=$(uname | tr '[:upper:]' '[:lower:]')
@@ -19,38 +14,55 @@ PWD=$(pwd)
 
 # Set the directory where the Caddy binary is located
 CADDY_DIR=$PWD
+STATIC=$CADDY_DIR/static
 
+# Test if UPSTREAM_HOST is a domain or IP address
 # Execute sed command
-sed "s|PORT|$PORT|g" Caddyfile.template >Caddyfile.stage1
-sed "s|UPSTREAM_HOST|$UPSTREAM_HOST|g" Caddyfile.stage1 >Caddyfile.stage2
-sed "s|UPSTREAM_PATTERN|$UPSTREAM_PATTERN|g" Caddyfile.stage2 >Caddyfile.stage3
-cp Caddyfile.stage3 Caddyfile
-rm -rf Caddyfile.stage*
-
-# Check the operating system and architecture, and set the appropriate permissions for the Caddy binary
-if [[ "$OS" == "darwin" && "$ARCH" == "arm64" ]]; then
-  CADDY_PROCESS_NAME="caddy_darwin_arm64"
-elif [[ "$OS" == "darwin" && "$ARCH" == "x86_64" ]]; then
-  CADDY_PROCESS_NAME="caddy_darwin_amd64"
-elif [[ "$OS" == "linux" && "$ARCH" == "x86_64" ]]; then
-  CADDY_PROCESS_NAME="caddy_linux_amd64"
+if [[ $UPSTREAM_HOST =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  sed -e "s|PORT|$PORT|g" -e "s|STATIC|$STATIC|g" -e "s|UPSTREAM_HOST|$UPSTREAM_HOST|g" -e "s|UPSTREAM_PATTERN|$UPSTREAM_PATTERN|g" caddy-file.json.template-ip.json >caddy-file.json
 else
-  echo "Unsupported operating system or architecture"
-  exit 1
+  sed -e "s|PORT|$PORT|g" -e "s|STATIC|$STATIC|g" -e "s|UPSTREAM_HOST|$UPSTREAM_HOST|g" -e "s|UPSTREAM_PATTERN|$UPSTREAM_PATTERN|g" caddy-file.json.template-address.json >caddy-file.json
 fi
 
+# Check the operating system and architecture, and set the appropriate permissions for the Caddy binary
+case "$OS" in
+darwin)
+  case "$ARCH" in
+  arm64) CADDY_PROCESS_NAME="caddy_darwin_arm64" ;;
+  x86_64) CADDY_PROCESS_NAME="caddy_darwin_amd64" ;;
+  *)
+    echo "Unsupported architecture"
+    exit 1
+    ;;
+  esac
+  ;;
+linux)
+  case "$ARCH" in
+  x86_64) CADDY_PROCESS_NAME="caddy_linux_amd64" ;;
+  *)
+    echo "Unsupported architecture"
+    exit 1
+    ;;
+  esac
+  ;;
+*)
+  echo "Unsupported operating system"
+  exit 1
+  ;;
+esac
+
 CADDY_BINARY=$CADDY_DIR"/"$CADDY_PROCESS_NAME
-chmod +x $CADDY_BINARY
+chmod +x "$CADDY_BINARY"
 
 # Function to start Caddy
 start_caddy() {
-  if pgrep -x "$CADDY_PROCESS_NAME" >/dev/null; then
+  if pgrep -f "$CADDY_PROCESS_NAME" >/dev/null; then
     echo "Caddy is already running."
   else
     if [ "$DAEMON" = true ]; then
-      nohup $CADDY_BINARY run 2>&1 >spit.log &
+      nohup "$CADDY_BINARY" run --config=./caddy-file.json 2>&1 >spit.log &
     else
-      $CADDY_BINARY run
+      "$CADDY_BINARY" run --config=./caddy-file.json
     fi
     echo "Caddy started."
   fi
@@ -58,8 +70,8 @@ start_caddy() {
 
 # Function to stop Caddy
 stop_caddy() {
-  if pgrep -x "$CADDY_PROCESS_NAME" >/dev/null; then
-    pkill -x "$CADDY_PROCESS_NAME"
+  if pgrep -f "$CADDY_PROCESS_NAME" >/dev/null; then
+    pkill -f "$CADDY_PROCESS_NAME"
     echo "Caddy stopped."
   else
     echo "Caddy is not running."
@@ -68,17 +80,11 @@ stop_caddy() {
 
 # Perform the specified action
 case "$ACTION" in
-start)
-  start_caddy
-  ;;
-stop)
-  stop_caddy
-  ;;
+start) start_caddy ;;
+stop) stop_caddy ;;
 daemon)
   DAEMON=true
   start_caddy
   ;;
-*)
-  echo "Usage: $0 [start|stop|daemon] port_number upstream_pattern] [upstream_host"
-  ;;
+*) echo "Usage: $0 [start|stop|daemon] [port_number] [upstream_pattern] [upstream_host]" ;;
 esac
