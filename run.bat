189eb3ab480e6861f11b17adf6f0ced298a69e9f
Noah Yao <noahyao1024@gmail.com>
2023-06-15 15:47:45 +0800

    Final done with run.bat.

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set PORT=%1
-if "%PORT%"=="" set PORT=80
+set "PORT=%1"
+if "%PORT%"=="" set "PORT=80"
 
-set STATIC=%2
-if "%STATIC%"=="" set STATIC=./static
+set "STATIC=%2"
+if "%STATIC%"=="" set "STATIC=./static"
 
-./caddy_windows_amd64.exe file-server --browse --listen :%PORT% --root %STATIC%
+caddy_windows_amd64.exe file-server --browse --listen :%PORT% --root %STATIC%
